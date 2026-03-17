a50fcaece70f57696e0539b9cbfcf77cb3a73728
MelekhFad <fadhilimgaya31@gmail.com>
2026-03-17 11:22:12 +0300

    Complete Quest 3: Variables Storing Data

---
+++ quest3-data-types.js
@@ -0,0 +1,16 @@
+// Creating variables
+let userName = "MelekhFad31";
+let userAge = 20;
+const userCountry = "Tanzania";
+
+// Logging values 
+console.log(username);
+console.log(userAge);
+console.log(userCountry);
+
+//Changing variable values
+userName = "KingFad31";
+console.log(userName);
+
+// Trying to change a constant ( DON'T DO THIS ❌)
+// userCountry = "Kenya"; // This will cause an error!
