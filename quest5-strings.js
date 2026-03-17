74cb24c613734748ea796ab3c6fcdcf66aa4adb5
MelekhFad <fadhilimgaya31@gmail.com>
2026-03-17 21:24:34 +0300

    Complete Quest 5: Selecting HTML elements using DOM methods

---
+++ quest5-strings.js
@@ -0,0 +1,11 @@
+// 🔹 Select element by ID
+const title = document.getElementById("title");
+console.log("Title:", title);
+
+// 🔹 Select first elememt with class
+const firstText = document.querySelector(".text");
+console.log("First paragraph:", firstText);
+
+// 🔹 Select all paragraphs
+const allParagraphs = document.querySelectorAll("p");
+console.log("All paragraphs:", allParagraphs);
